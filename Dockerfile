7dd3e2e8d0a69e613352dc8dabea36966b690b15
qingcloud-cps-robot <cps-robot@yunify.com>
2022-03-26 10:36:04 +0800

    add workflow for qingcloud sdk

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
-FROM alpine:latest
+FROM ubuntu:latest
 
-RUN apk add --no-cache git
+RUN  apt-get install -y git
+RUN  apt-get install -y wget
+RUN  apt-get install -y golang
+RUN  apt-get install -y ruby-full
+RUN  gem install rufo
 
+COPY snips /usr/bin/snips
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,98 +4,55 @@ set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 
 echo "[+] Action start"
-SOURCE_BEFORE_DIRECTORY="${1}"
-SOURCE_DIRECTORY="${2}"
-DESTINATION_GITHUB_USERNAME="${3}"
-DESTINATION_REPOSITORY_NAME="${4}"
-GITHUB_SERVER="${5}"
-USER_EMAIL="${6}"
-USER_NAME="${7}"
-DESTINATION_REPOSITORY_USERNAME="${8}"
-TARGET_BRANCH="${9}"
-COMMIT_MESSAGE="${10}"
-TARGET_DIRECTORY="${11}"
 
-if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
-then
-	DESTINATION_REPOSITORY_USERNAME="$DESTINATION_GITHUB_USERNAME"
-fi
 
-if [ -z "$USER_NAME" ]
-then
-	USER_NAME="$DESTINATION_GITHUB_USERNAME"
-fi
+USER_EMAIL="${1}"
+USER_NAME="${2}"
+DESTINATION_REPOSITORY_USERNAME="${3}"
 
-CLONE_DIR=$(mktemp -d)
+TARGET_API_SPECS_BRANCH="${4}"
+TARGET_BRANCH_SDK_MAIN="${5}"
+TARGET_BRANCH_SDK_SYNC="${6}"
 
-echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
-# Setup git
-git config --global user.email "$USER_EMAIL"
-git config --global user.name "$USER_NAME"
+TARGET_REPOSITORY_NAME_API_SPECS="${7}"
+TARGET_REPOSITORY_NAME_JAVA_SDK="${8}"
+TARGET_REPOSITORY_NAME_GO_SDK="${9}"
+TARGET_REPOSITORY_NAME_RUBY_SDK="${10}"
 
-{
-	git clone --single-branch --branch "$TARGET_BRANCH" "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
-} || {
-	echo "::error::Could not clone the destination repository. Command:"
-	echo "::error::git clone --single-branch --branch $TARGET_BRANCH https://$USER_NAME:the_api_token@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git $CLONE_DIR"
-	echo "::error::(Note that the USER_NAME and API_TOKEN is redacted by GitHub)"
-	echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the API_TOKEN_GITHUB"
-	exit 1
-
-}
-ls -la "$CLONE_DIR"
-
-TEMP_DIR=$(mktemp -d)
-# This mv has been the easier way to be able to remove files that were there
-# but not anymore. Otherwise we had to remove the files from "$CLONE_DIR",
-# including "." and with the exception of ".git/"
-mv "$CLONE_DIR/.git" "$TEMP_DIR/.git"
-
-# $TARGET_DIRECTORY is '' by default
-ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
-
-echo "[+] Deleting $ABSOLUTE_TARGET_DIRECTORY"
-rm -rf "$ABSOLUTE_TARGET_DIRECTORY"
-
-echo "[+] Creating (now empty) $ABSOLUTE_TARGET_DIRECTORY"
-mkdir -p "$ABSOLUTE_TARGET_DIRECTORY"
-
-echo "[+] Listing Current Directory Location"
-ls -al
-
-echo "[+] Listing root Location"
-ls -al /
-
-mv "$TEMP_DIR/.git" "$CLONE_DIR/.git"
-
-echo "[+] List contents of $SOURCE_DIRECTORY"
-ls "$SOURCE_DIRECTORY"
-
-echo "[+] Checking if local $SOURCE_DIRECTORY exist"
-if [ ! -d "$SOURCE_DIRECTORY" ]
-then
-	echo "ERROR: $SOURCE_DIRECTORY does not exist"
-	echo "This directory needs to exist when push-to-another-repository is executed"
-	echo
-	echo "In the example it is created by ./build.sh: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L19"
-	echo
-	echo "If you want to copy a directory that exist in the source repository"
-	echo "to the target repository: you need to clone the source repository"
-	echo "in a previous step in the same build section. For example using"
-	echo "actions/checkout@v2. See: https://github.com/cpina/push-to-another-repository-example/blob/main/.github/workflows/ci.yml#L16"
-	exit 1
-fi
-
-echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $TARGET_DIRECTORY in git repo $DESTINATION_REPOSITORY_NAME"
-cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$TARGET_DIRECTORY"
-cd "$CLONE_DIR"
-
-echo "[+] Files that will be pushed"
-ls -la
+COMMIT_MESSAGE="${11}"
 
 ORIGIN_COMMIT="https://$GITHUB_SERVER/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
+
+CLONE_DIR=$(mktemp -d)
+
+# Setup git
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$USER_NAME"
+
+echo "[+] Cloning $TARGET_REPOSITORY_NAME_API_SPECS"
+git clone --single-branch --branch "$TARGET_API_SPECS_BRANCH" "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$TARGET_REPOSITORY_NAME_API_SPECS.git" "$CLONE_DIR"
+
+ls -la "$CLONE_DIR"
+
+echo "[+] Cloning $TARGET_REPOSITORY_NAME_JAVA_SDK"
+git clone --single-branch --branch "$TARGET_BRANCH_SDK_MAIN" "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$TARGET_REPOSITORY_NAME_JAVA_SDK.git" "$CLONE_DIR"
+
+
+ls -la $CLONE_DIR/$TARGET_REPOSITORY_NAME_JAVA_SDK
+
+echo "[+] Update $TARGET_REPOSITORY_NAME_JAVA_SDK by snips"
+snips -f $CLONE_DIR/qingcloud-api-specs/2013-08-30/swagger/api_v2.0.json -t $CLONE_DIR/qingcloud-sdk-java/tmpl -o $CLONE_DIR/qingcloud-sdk-java/src/main/java/com/qingcloud/sdk/service/
+
+
+echo "[+] Push $TARGET_REPOSITORY_NAME_JAVA_SDK to $TARGET_BRANCH_SDK_SYNC"
+
+cd "$CLONE_DIR/$TARGET_REPOSITORY_NAME_JAVA_SDK"
+
+
+echo "[+] Files that will be pushed"
+ls -la
 
 echo "[+] Adding git commit"
 git add .
@@ -109,4 +66,64 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
 echo "[+] Pushing git commit"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
-git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --set-upstream "$TARGET_BRANCH"
+git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$TARGET_REPOSITORY_NAME_JAVA_SDK.git" --set-upstream "$TARGET_BRANCH_SDK_SYNC"
+
+
+echo "[+] Cloning $TARGET_REPOSITORY_NAME_GO_SDK"
+git clone --single-branch --branch "$TARGET_BRANCH_SDK_MAIN" "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$TARGET_REPOSITORY_NAME_GO_SDK.git" "$CLONE_DIR"
+
+snips -f ./qingcloud-api-specs/2013-08-30/swagger/api_v2.0.json -t ./qingcloud-sdk-go/template -o ./qingcloud-sdk-go/service/
+gofmt -w .
+
+cd "$CLONE_DIR/$TARGET_REPOSITORY_NAME_GO_SDK"
+
+
+echo "[+] Push $TARGET_REPOSITORY_NAME_GO_SDK to $TARGET_BRANCH_SDK_SYNC"
+cd "$CLONE_DIR/$TARGET_REPOSITORY_NAME_GO_SDK"
+
+echo "[+] Files that will be pushed"
+ls -la
+
+echo "[+] Adding git commit"
+git add .
+
+echo "[+] git status:"
+git status
+
+echo "[+] git diff-index:"
+# git diff-index : to avoid doing the git commit failing if there are no changes to be commit
+git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
+
+echo "[+] Pushing git commit"
+# --set-upstream: sets de branch when pushing to a branch that does not exist
+git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$TARGET_REPOSITORY_NAME_GO_SDK.git" --set-upstream "$TARGET_BRANCH_SDK_SYNC"
+
+
+
+echo "[+] Cloning $TARGET_REPOSITORY_NAME_RUBY_SDK"
+git clone --single-branch --branch "$TARGET_BRANCH_SDK_MAIN" "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$TARGET_REPOSITORY_NAME_RUBY_SDK.git" "$CLONE_DIR"
+
+echo "[+] Update $TARGET_REPOSITORY_NAME_RUBY_SDK by snips"
+snips -f ./qingcloud-api-specs/2013-08-30/swagger/api_v2.0.json -t ./qingcloud-sdk-ruby/template -o ./qingcloud-sdk-ruby/lib/qingcloud/sdk/service/
+rufo ./lib/qingcloud/sdk/service/*
+
+
+echo "[+] Push $TARGET_REPOSITORY_NAME_RUBY_SDK to $TARGET_BRANCH_SDK_SYNC"
+cd "$CLONE_DIR/$TARGET_REPOSITORY_NAME_RUBY_SDK"
+
+echo "[+] Files that will be pushed"
+ls -la
+
+echo "[+] Adding git commit"
+git add .
+
+echo "[+] git status:"
+git status
+
+echo "[+] git diff-index:"
+# git diff-index : to avoid doing the git commit failing if there are no changes to be commit
+git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
+
+echo "[+] Pushing git commit"
+# --set-upstream: sets de branch when pushing to a branch that does not exist
+git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$TARGET_REPOSITORY_NAME_RUBY_SDK.git" --set-upstream "$TARGET_BRANCH_SDK_SYNC"
